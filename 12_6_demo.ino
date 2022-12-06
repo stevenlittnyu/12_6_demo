fd33a0b9b8cdb99f7de26a5bbb1ec27bd56792fa
stevenlittnyu <75270947+stevenlittnyu@users.noreply.github.com>
2022-12-06 11:35:16 -0500

    added led blink

--- 12_6_demo.ino
+++ 12_6_demo.ino
@@ -2,10 +2,11 @@ int ledPin = 13;
 
 void setup() {
   pinMode(ledPin, OUTPUT);
-
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(ledPin, HIGH);
+  delay(500);
+  digitalWrite(ledPin, LOW);
+  delay(500);
 }
